bd485d88e1f0fe5be5022ae2cffc96e90dcc8b36
Michael Lai <themichaellai@gmail.com>
2014-03-29 03:09:32 -0400

    Dumb http auth

--- rapid_response_kit/app.py
+++ rapid_response_kit/app.py
@@ -10,8 +10,10 @@ from rapid_response_kit.tools import ringdown
 from rapid_response_kit.tools import simplehelp
 from rapid_response_kit.tools import survey
 from rapid_response_kit.tools import town_hall
+from flask.ext.httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 app.config.from_object('rapid_response_kit.utils.config')
 
 app.config.apps = Registry()
@@ -27,7 +29,14 @@ simplehelp.install(app)
 survey.install(app)
 town_hall.install(app)
 
+@auth.get_password
+def get_pw(username):
+    if username == app.config['USER']:
+        return app.config['PASSWORD']
+    return None
+
 @app.route('/')
+@auth.login_required
 def home():
     return render_template('home.html')
 
